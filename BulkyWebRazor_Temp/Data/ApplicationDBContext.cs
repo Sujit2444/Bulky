30bd89455dc2d4ffbedc2bdf870fdb4884bf0cd1
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-04-06 18:00:41 +0600

    Create database and table for bulky razor pages

---
+++ BulkyWebRazor_Temp/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}

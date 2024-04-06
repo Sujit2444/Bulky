1825cf8c8f370197dd28698342ce80d06d707657
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-04-06 23:33:40 +0600

    Display all categories for razor pages

--- BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,17 @@ namespace BulkyWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDBContext _db;
+
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDBContext applicationDBContext)
+        {
+           _db= applicationDBContext;
+        }
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
